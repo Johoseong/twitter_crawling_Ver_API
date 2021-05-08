28ff96150286785b053fca7805ca18d58cbe4d93
thisisyoobi <thisisyoobi@gmail.com>
2021-05-09 01:32:06 +0900

    update data

--- twitter_crawling_Ver_API.py
+++ twitter_crawling_Ver_API.py
@@ -17,7 +17,7 @@ class Crawling:
 
         # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
         # expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
-        self.query_params = {'query': '', 'tweet.fields': 'created_at', 'start_time': '2020-01-01T00:00:00Z',
+        self.query_params = {'query': '', 'tweet.fields': 'created_at', 'start_time': '2016-01-01T00:00:00Z',
                         'end_time': '2021-01-01T00:00:00Z', 'max_results': '500'}
 
     def create_headers(self):
@@ -32,11 +32,20 @@ class Crawling:
         return response.json()
 
     def main_act(self, brand_list, drug_name):
-        fw = open(drug_name + " " + self.query_params['start_time'][0:10] + "~" + self.query_params['end_time'][0:10] + ".txt", "w")
-        for brand_name in brand_list:
-            self.query_params['query'] = brand_name
-            # print(self.query_params)
-            self.crawling_part(fw)
+        for year in range(2019, 2020):
+            for month in range(12, 13):
+                if month == 13:
+                    self.query_params['end_time'] = str(year + 1) + "-" + str(1).zfill(2) + "-01T00:00:00Z"
+                    self.query_params['start_time'] = str(year) + "-" + str(month - 1).zfill(2) + "-01T00:00:00Z"
+                else:
+                    self.query_params['end_time'] = str(year) + "-" + str(month).zfill(2) + "-01T00:00:00Z"
+                    self.query_params['start_time'] = str(year) + "-" + str(month - 1).zfill(2) + "-01T00:00:00Z"
+                fw = open(drug_name + " " + self.query_params['start_time'][0:10] + "~" + self.query_params['end_time'][0:10] + ".txt", "w")
+                for brand_name in brand_list:
+                    self.query_params['query'] = brand_name
+                    print(self.query_params)
+                    self.crawling_part(fw)
+                fw.close()
 
     def crawling_part(self, fw):
         headers = self.create_headers()
@@ -47,6 +56,8 @@ class Crawling:
             self.query_params['next_token'] = json_response['meta']['next_token']
         except Exception as e:
             print(self.query_params['query'] + " done")
+            self.query_params['next_token'] = "DUMMY"
+            del self.query_params['next_token']
             # print(e)
             time.sleep(2)
             return
--- twitter_main_activity.py
+++ twitter_main_activity.py
@@ -7,7 +7,7 @@ main_crawling = twitter_crawling_Ver_API.Crawling()
 # print(drug_brand.brandname_lists)
 # print(drug_brand.columns)
 
-for i in range(len(drug_brand.columns)):
+for i in range(0, 1): #len(drug_brand.columns)):
     print(drug_brand.columns[i] + " start")
     main_crawling.main_act(drug_brand.brandname_lists[i], drug_brand.columns[i])
     print(drug_brand.columns[i] + " done")
